00fd5ad12478da1c494801526c84b4d4376be01c
Artem <azaner228@gmail.com>
2020-12-22 15:31:59 +0300

    Поменял корутины на mathf.MoveTowards

--- Assets/Scripts/SoundValueChanger.cs
+++ Assets/Scripts/SoundValueChanger.cs
@@ -7,45 +7,39 @@ public class SoundValueChanger : MonoBehaviour
     [SerializeField] private float _durationOfAlarmActivation;
 
     private AudioSource _currentAudio;
-    private WaitForSeconds _waitForAnySeconds;
+    private StateOfSound _stateOfCurrentSound;
 
     private void Start()
-    { 
-       _waitForAnySeconds = new WaitForSeconds(_durationOfAlarmActivation / 100);
+    {
+        _stateOfCurrentSound = StateOfSound.Empty;
     }
+
+    private void Update()
+    {
+        if (_stateOfCurrentSound == StateOfSound.Increase)
+            _currentAudio.volume = Mathf.MoveTowards(_currentAudio.volume, 1, _durationOfAlarmActivation * Time.deltaTime);
+        else if (_stateOfCurrentSound == StateOfSound.Decrease)
+            _currentAudio.volume = Mathf.MoveTowards(_currentAudio.volume, 0, _durationOfAlarmActivation * Time.deltaTime);
+
+    }
+
     public void IncreaseSoundVolume(AudioSource audioSource)
     {
-        _currentAudio = audioSource;
+        _stateOfCurrentSound = StateOfSound.Increase;
 
-        var increaseSound = StartCoroutine(IncreaseSoundVolume());
+        _currentAudio = audioSource;
     }
 
     public void DecreaseSoundVolume(AudioSource audioSource)
     {
+        _stateOfCurrentSound = StateOfSound.Decrease;
+
         _currentAudio = audioSource;
-
-        var decreaseSound = StartCoroutine(DecreaseAudioVolume());
     }
-
-    private IEnumerator IncreaseSoundVolume()
-    {
-        for (float i = 0; i <= 1f; i += 0.01f)
-        {
-            _currentAudio.volume = 1 * i;
-
-            yield return _waitForAnySeconds;
-        }
-    }
-
-    private IEnumerator DecreaseAudioVolume()
-    {
-        for (float i = 1; i >= 0f; i -= 0.01f)
-        {
-            _currentAudio.volume = 1 * i;
-
-            yield return _waitForAnySeconds;
-        }
-        _currentAudio.volume = 0;
-        _currentAudio.Stop();
-    }
+}
+enum StateOfSound
+{
+    Decrease,
+    Increase,
+    Empty
 }
