7effc671fd59fbed53397075ef36eef6b84b87e2
Artem <azaner228@gmail.com>
2020-12-26 13:14:30 +0300

    Man

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    private Transform[] _enemySpawnPoints;
+    [SerializeField] private float _frequency = 2;
+
+    private void Start()
+    {
+        _enemySpawnPoints = GetComponentsInChildren<Transform>();
+
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(_frequency);
+
+        while (true)
+        {
+            int randomIndex = Random.Range(0, _enemySpawnPoints.Length);
+
+            Instantiate(_enemy, _enemySpawnPoints[randomIndex].position, Quaternion.identity);
+
+            yield return waitForSeconds;
+        }
+    }
+}
